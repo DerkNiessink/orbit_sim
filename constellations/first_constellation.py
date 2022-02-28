41a59a2fc4da53eed3f2bfb5b32566d26605e2ce
Frank Niessink <frank@niessink.com>
2022-03-01 00:17:39 +0100

    Fix center on click. Add radius to constellation file. (#28)

--- constellations/first_constellation.py
+++ constellations/first_constellation.py
@@ -1,10 +1,12 @@
-AU = 149_597_871 * 10 ** 3
+# Note, all measures are in SI units (kg, m, s, etc.)
 
+AU = 149_597_871 * 10 ** 3
 
 constellation = {
     "Sun": {
         "x": 0,
         "y": 0,
+        "radius": 696_342_000,
         "init_velocity_x": 0,
         "init_velocity_y": 0,
         "mass": 1.98847 * 10 ** 30,
@@ -14,8 +16,9 @@ constellation = {
     "Earth": {
         "x": -AU,
         "y": 0,
+        "radius": 6_371_000,
         "init_velocity_x": 0,
-        "init_velocity_y": 29800,
+        "init_velocity_y": 29_800,
         "mass": 5.772 * 10 ** 24,
         "colour": [0, 0, 250],
         "image": "resources/planet1.png",
@@ -23,6 +26,7 @@ constellation = {
     "Mars": {
         "x": -249_000_000_000,
         "y": 0,
+        "radius": 3_389_500,
         "init_velocity_x": 0,
         "init_velocity_y": 21970,
         "mass": 6.39 * 10 ** 23,
@@ -32,6 +36,7 @@ constellation = {
     "Pluto": {
         "x": -7304.326 * 10 ** 9,
         "y": 0,
+        "radius": 1_188_300,
         "init_velocity_x": 0,
         "init_velocity_y": 3710,
         "mass": 0.01303 * 10 ** 24,
