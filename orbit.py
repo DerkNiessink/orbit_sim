7998e93cc8ac8d41de6665244174ede53e4025de
Frank Niessink <frank@niessink.com>
2022-03-01 22:11:37 +0100

    Move camera to separate module. (#36)

--- orbit.py
+++ orbit.py
@@ -5,7 +5,7 @@ import pygame
 from constellations.first_constellation import constellation, general_parameters, AU
 from physicalobject_model import PhysicalObjectModel
 from physicalobject_views import PhysicalObjectView
-
+from camera import Camera
 
 width = 1500
 height = 800
@@ -15,7 +15,6 @@ game_window = pygame.display.set_mode((width, height))
 game_window.fill((50, 50, 50))
 pygame.display.set_caption("orbit simulator")
 pygame.init()
-font = pygame.font.SysFont("monospace", 18)
 keys = pygame.key.get_pressed()
 elapsed_time = 0
 
@@ -44,82 +43,7 @@ for body in constellation:
     )
 
 
-class Camera:
-
-    MAX_ZOOM_LEVEL = 10000
-    MIN_ZOOM_LEVEL = 0.1
-    ZOOM_STEP = 1.1
-
-    def __init__(self, width, height, body):
-        self.width = width
-        self.height = height
-        self.zoomLevel = 1
-        self.elapsed_time_to_draw = 0
-        self.bodyToTrack = body
-
-    def trackBody(self, body):
-        self.bodyToTrack = body
-
-    def zoomIn(self):
-        """Zoom in to a maximum of 0.1."""
-        self.zoomLevel = max(self.zoomLevel / self.ZOOM_STEP, self.MIN_ZOOM_LEVEL)
-
-    def zoomOut(self):
-        """Zoom in to a maximum of 10."""
-        self.zoomLevel = min(self.zoomLevel * self.ZOOM_STEP, self.MAX_ZOOM_LEVEL)
-
-    def update(self, window):
-
-        # fill camera background black
-        window.fill((0, 0, 0))
-
-        # update the positions for each body
-        for body in body_models:
-            body.update_position(body_models)
-
-        # update camera
-        x, y = self.bodyToTrack.get_position_pixels()
-        cameraX = x + self.width / 2
-        cameraY = y + self.height / 2
-
-        # calculate offsets
-        offsetX = self.width / 2 - (cameraX * self.zoomLevel)
-        offsetY = self.height / 2 - (cameraY * self.zoomLevel)
-
-        # render bodies
-        for body in body_viewers:
-            body.draw(window, offsetX, offsetY, self.width, self.height, self.zoomLevel)
-
-        # draw the elapsed time in steps of 10 days
-        if int(elapsed_time) % 10 == 0:
-            self.elapsed_time_to_draw = elapsed_time
-        if elapsed_time < 365:
-            label_time = font.render(
-                f"Elapsed time: {int(self.elapsed_time_to_draw)} days",
-                1,
-                (255, 255, 255),
-            )
-        else:
-            label_time = font.render(
-                f"Elapsed time: {round(int(self.elapsed_time_to_draw) / 365, 1)} yr",
-                1,
-                (255, 255, 255),
-            )
-        game_window.blit(label_time, (25, 25))
-
-        # draw the scale on the screen
-        pixel_size = 0.026  # cm
-        label_zoom = font.render(
-            f"Scale: {round(body_viewers[0].scale_factor * AU * camera.zoomLevel * pixel_size, 2)} cm = AU",
-            1,
-            (255, 255, 255),
-        )
-        game_window.blit(label_zoom, (25, 48))
-
-
-# Set the default body to track
-body_to_track = body_viewers[0]
-camera = Camera(width, height, body_to_track)
+camera = Camera(game_window, body_models, body_viewers)
 
 if __name__ == "__main__":
     run = True
@@ -155,7 +79,7 @@ if __name__ == "__main__":
         elapsed_time += general_parameters["time_step"] / (3600 * 24)
 
         # update the camera system and draw bodies
-        camera.update(game_window)
+        camera.update(elapsed_time)
 
         pygame.display.update()
 
