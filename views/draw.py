6414398a0ce6e1e42b47a0b97c3f78271b52e99d
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-03-29 23:41:45 +0200

    Update draw.py

--- views/draw.py
+++ views/draw.py
@@ -34,7 +34,7 @@ class Line(Drawable):
     def __init__(self, screen_positions: Sequence[Vector3], colour: tuple[int, int, int]) -> None:
         super().__init__(screen_positions)
         self.colour = colour
-        self.width = 5
+        self.width = 2
 
     def draw(self, window: Surface) -> None:
         """Draw the drawable on the window."""
