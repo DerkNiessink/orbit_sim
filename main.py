456728d3759dbc9c1d296d7f9602b75e1bda5df6
DerkNiessink <94176246+DerkNiessink@users.noreply.github.com>
2022-05-02 01:37:56 +0200

    Body type can be changed with ComboBox

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from multiprocessing import Process
 import sys
 import os
+from functools import partial
 
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QTableWidgetItem, QPushButton
+from PyQt5.QtWidgets import QTableWidgetItem, QComboBox
 import qdarktheme
 import numpy as np
 import json
 
 from sim import orbit_sim
+from resources.image_type import images
 from models.physicalobject import elements_to_cartesian
 
 AU = 149_597_871 * 10 ** 3
@@ -22,43 +24,53 @@ class OrbitSimGui(QtWidgets.QMainWindow):
             open("orbit_sim.ui"),
             self,
         )
-            
-        self.example_constellations = [filename.strip(".json") for filename in os.listdir("constellations") if ".json" in filename]
+
+        # Load in the names of the saved constellations in the resources repository
+        self.constellations = [filename.strip(".json") for filename in os.listdir("constellations") if ".json" in filename]
+
         self.conversion = Conversion(self.tableWidget, self.const_ComboBox)
-        self.start_PushButton.clicked.connect(self.start_orbit)
-        self.const_ComboBox.addItems(self.example_constellations)
+
+        # Make dict for the type-ComboBoxes per constellation
+        self.types_ComboBoxes = {} 
+        for constellation in self.constellations:
+            self.types_ComboBoxes[constellation] = {}
+
+        # Slots and signals
+        self.start_PushButton.clicked.connect(self.start_sim)
+        self.const_ComboBox.addItems(self.constellations)
         self.const_ComboBox.currentTextChanged.connect(self.show_current_constellation)
         self.const_ComboBox.textActivated.connect(self.make_uneditable)
         self.show_current_constellation()
         self.set_header()
         self.add_PushButton.clicked.connect(self.add_body)
         self.add_const_PushButton.clicked.connect(self.add_constellation)
-        self.tableWidget.itemChanged.connect(self.conversion.table_to_json)
+        self.tableWidget.itemChanged.connect(partial(self.conversion.table_to_json, self.types_ComboBoxes))
         self.delete_PushButton.clicked.connect(self.delete_body)
 
-    def start_orbit(self):
+    def start_sim(self):
+        """Start the simulation"""
         p = Process(target=orbit_sim, args=(f"./constellations/{self.const_ComboBox.currentText()}.json",))
         p.start()
 
     def show_current_constellation(self):
+        """Show the selected constelllation in the table"""
         self.tableWidget.clear()
         self.tableWidget.setRowCount(0)
         self.set_header()
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setColumnWidth(1, 150)
-        if self.const_ComboBox.currentText() in self.example_constellations:
+        if self.const_ComboBox.currentText() in self.constellations:
             self.make_uneditable()
-
             constellation = self.conversion.open_current_constellation()
             self.const_to_table(constellation)
 
     def const_to_table(self, constellation) -> None:
         """add the constellation items to the table"""
-        for name, body in constellation["Constellation"].items():
-            self.column = 0
-            self.tableWidget.insertRow(0)
-            self.tableWidget.setItem(0, 0, QTableWidgetItem(name))
 
+        # Iterate over reversed dict, so that order of the table stays the same
+        for name, body in reversed(constellation["Constellation"].items()):
+            self.column = 0
+            
             # determine if user input was orbital elements or cartesian
             init_position = body.get("init_position")
             init_velocity = body.get("init_velocity")
@@ -66,28 +78,43 @@ class OrbitSimGui(QtWidgets.QMainWindow):
                 init_position, init_velocity = elements_to_cartesian(body["aphelion"], body["min_orbital_velocity"], body["inclination"])
 
             # add body parameters to columns
+            self.add_body()
+            self.addItem(name)
             self.addItem(tuple([round(x/AU, 2) for x in init_position]))
             self.addItem(tuple([round(x/1000,2) for x in init_velocity]))
             self.addItem(round(body["radius"]/1000))
             self.addItem(scientific(body["mass"]))
-            self.addItem(body["type"])
+            self.types_ComboBoxes[self.const_ComboBox.currentText()][self.tableWidget.rowCount()].setCurrentText(body["type"])
+            self.column +=1 
             self.addItem(body.get("tail_length", 5000))
 
     def add_body(self) -> None:
-        self.tableWidget.insertRow(self.tableWidget.rowCount())
+        """Add an empty (except types-combobox) row to the table"""
+        self.tableWidget.insertRow(0)
+        self.addTypes()
 
     def make_uneditable(self) -> None:
         self.const_ComboBox.setEditable(False)
 
     def add_constellation(self) -> None:
+        """Add an empty constellation to the constellations combobox"""
         self.const_ComboBox.setEditable(True)
         self.const_ComboBox.setInsertPolicy(self.const_ComboBox.InsertAtTop)
         self.const_ComboBox.setCurrentIndex(0)
 
+    def addTypes(self) -> None:
+        """Add the combobox containing the body types to column 5"""
+        index = self.tableWidget.rowCount()
+        constellation = self.const_ComboBox.currentText()
+        self.types_ComboBoxes[constellation][index] = QComboBox()
+        self.types_ComboBoxes[constellation][index].addItems([key for key in images])
+        self.types_ComboBoxes[constellation][index].currentTextChanged.connect(partial(self.conversion.table_to_json, self.types_ComboBoxes))
+        self.tableWidget.setCellWidget(0, 5, self.types_ComboBoxes[constellation][index])
+
     def addItem(self, input) -> None:
         """add an item to the current column"""
-        self.column += 1
         self.tableWidget.setItem(0, self.column, QTableWidgetItem(f"{input}"))
+        self.column += 1
 
     def set_header(self) -> None:
         self.tableWidget.setHorizontalHeaderLabels(["Name" ,"Position (AU)", "Velocity (km/s)", "Radius (km)", "Mass (kg)", "Type", "Tail Length (px)"])
@@ -106,31 +133,30 @@ class OrbitSimGui(QtWidgets.QMainWindow):
         self.conversion.save_constellation(constellation)
 
 
-
 class Conversion:
     """Class for converting and saving the table as a json file"""
 
-    def __init__(self, table, comboBox):
+    def __init__(self, table, ComboBox):
         self.table = table
-        self.comboBox = comboBox
+        self.ComboBox = ComboBox
 
     def save_constellation(self, constellation) -> None:
         """save dict as json file"""
-        with open(f"./constellations/{self.comboBox.currentText()}.json", "w") as fp:
+        with open(f"./constellations/{self.ComboBox.currentText()}.json", "w") as fp:
             json.dump(constellation, fp, indent = 4)
 
     def open_current_constellation(self) -> None:
         """open json file as dict"""
-        with open(f"./constellations/{self.comboBox.currentText()}.json") as json_file:
+        with open(f"./constellations/{self.ComboBox.currentText()}.json") as json_file:
             return json.load(json_file)
 
-    def table_to_json(self) -> None:
+    def table_to_json(self, types_ComboBoxes) -> None:
         """if updated, save data in table as json file"""
 
         # Check if all columns are filled
         for row in range(self.table.rowCount()):
             for column in range(self.table.columnCount()):
-                if self.table.item(row, column) == None:
+                if self.table.item(row, column) == None and column != 5:
                     return
 
         constellation = dict(Constellation=dict())
@@ -139,7 +165,10 @@ class Conversion:
         for row in range(self.table.rowCount()):
             body = {}
             for column in range(1, self.table.columnCount()):
-                body[f"{self.table.horizontalHeaderItem(column).text()}"] = str(self.table.item(row, column).text())
+                if self.table.horizontalHeaderItem(column).text() == "Type":
+                    body["Type"] = types_ComboBoxes[self.ComboBox.currentText()][self.table.rowCount()-row].currentText() # rowCount - row because ComboBoxes were added in reverse order.
+                else:
+                    body[f"{self.table.horizontalHeaderItem(column).text()}"] = str(self.table.item(row, column).text())
             constellation["Constellation"][f"{self.table.item(row, 0).text()}"] = body
 
         self.convert_to_data(constellation["Constellation"])
